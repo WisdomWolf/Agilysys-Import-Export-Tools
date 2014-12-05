e38043c61e317d4cb17652f6eafa8fe9dd4434f9
WisdomWolf <WisdomWolf@gmail.com>
2014-12-05 16:30:35 -0500

    added separated files for import/export

--- AgilysysExportSimplifier.py
+++ AgilysysExportSimplifier.py
@@ -39,7 +39,6 @@ def fixArray(match):
 	match = str(match.group(0))
 	return match.replace(",",";")
 	
-
 def preParse():
 	for x in export:
 		itemDetails = re.sub(priceArrayMatch, fixArray, x)
@@ -62,38 +61,6 @@ def generateSimpleExport(items=itemList, altered=True):
 				simpleOutput.write(str(item.id) + "," + str(item.name) + "," + str(item.priceLevels) + "\r\n")
 		else:
 			simpleOutput.write(str(item.id) + "," + str(item.name) + "," + str(item.priceLevels) + "\r\n")
-
-def generateIGPriceUpdate(items=None):
-	file_opt = options = {}
-	options['defaultextension'] = '.txt'
-	options['filetypes'] = [('text files', '.txt'), ('all files', '.*')]
-	options['title'] = 'Open Simplified Export'
-
-	file_path = filedialog.askopenfilename(**file_opt)
-	if file_path == None or file_path == "":
-		print("No file selected")
-		return
-	inputFile = codecs.open(file_path, 'r', 'utf8')
-	while(True):
-		save_path = filedialog.askdirectory()
-		if save_path != None and save_path != "":
-			save_file = str(save_path) + "/MI_IMP.txt"
-		else:
-			print("No file selected")
-			return
-		try:
-			updateFile = codecs.open(save_file, 'x', 'utf8')
-			break
-		except FileExistsError:
-			print("There is already an Agilysys import file in this directory.  Please try again.")
-	
-	for x in inputFile:
-		details = x.split(",")
-		details[2] = details[2].replace(";", ",").strip("\r\n")
-		line = '"U",' + str(details[0]) + ',,,,,' + str(details[2]) + ',,,,,,,,,,,,,,,,,\r\n'
-		updateFile.write(line)
-	
-	print("File output written successfully")
 
 preParse()
 generateSimpleExport()

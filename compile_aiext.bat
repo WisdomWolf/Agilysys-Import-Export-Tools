6e5d0c36086449a079b0ab0cdeca3490cfb8be1c
WisdomWolf <WisdomWolf@gmail.com>
2015-12-03 17:44:13 -0500

    updated build script

--- compile_aiext.bat
+++ compile_aiext.bat
@@ -1,7 +1,17 @@
 @Echo OFF
+rem ***** get rid of all the old files in the build folder
+RD /S /Q build
 
-pyinstaller --onefile "Agilysys Import Export Tool.spec"
+rem ***** ensure clean dist folder
+RD /S /Q dist
+
+python setup.py py2exe
 CD dist
-7z a AIEXT.zip *.exe
+7z a AIEXT.zip *.*
 PAUSE
+rem ***** cleanup temp files
 DEL *.exe
+DEL *.dll
+DEL library.zip
+
+rem ***** Need to adjust to include folders in zip
